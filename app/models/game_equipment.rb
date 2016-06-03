8b80b128f934f7c582a4d3f871cca1a0d101ceb8
Maynard Landrito <landritom@yahoo.com>
2016-06-03 12:07:49 +0800

    change to 5

--- app/models/game_equipment.rb
+++ app/models/game_equipment.rb
@@ -2,7 +2,7 @@ class GameEquipment < ActiveRecord::Base
   belongs_to :user
   
   def total_weight
-    a = 3
+    a = 5
     if weight.present? && quantity.present?
       weight * quantity
     else
